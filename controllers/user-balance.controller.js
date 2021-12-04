ca4b6f50d0600444b0bc1fc669ad4e461163442b
Lars Berger <lars.berger@yahoo.com>
2021-12-04 18:02:21 +0100

    feat: get user's balance in terms of USD in user balance controller

--- controllers/user-balance.controller.js
+++ controllers/user-balance.controller.js
@@ -1,3 +1,5 @@
+const { default: axios } = require('axios');
+
 const mockUserCache = {
   'user-1': {
     BTC: '0.5',
@@ -19,13 +21,35 @@ function getUserBalances(userId) {
   return mockUserCache[userId];
 }
 
+function getUsdCurrencyPair(currency) {
+  return `${currency.toLowerCase()}usd`;
+}
+
+async function getLatestCurrencyPairPrice(currencyPair) {
+  const response = await axios.get(`https://www.bitstamp.net/api/v2/ticker/${currencyPair}`);
+
+  return Number(response.data.last);
+}
+
 const userBalanceController = async (req, res, next) => {
   try {
     const { userId } = req.params;
 
+    // Get user's balances from cache.
     const userBalances = getUserBalances(userId);
 
-    res.send(userBalances);
+    // Keep running total of user's assets in terms of USD.
+    let totalBalance = 0;
+
+    for await (const [currency, userBalance] of Object.entries(userBalances)) {
+      // Get latest coin price in terms of USD.
+      const usdCurrencyPair = getUsdCurrencyPair(currency);
+      const latestPrice = await getLatestCurrencyPairPrice(usdCurrencyPair);
+
+      totalBalance += latestPrice * userBalance;
+    }
+
+    res.json({ totalBalance });
   } catch (e) {
     next({ code: 400, message: e.message });
   }
