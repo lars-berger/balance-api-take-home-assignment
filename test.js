d9523793982548a31bf6e0cbee868b1447c40da2
Lars Berger <lars.berger@yahoo.com>
2021-12-04 19:24:08 +0100

    test: add test cases for for error response and valid Bitstamp request

--- test.js
+++ test.js
@@ -1,9 +1,42 @@
-import chai from "chai";
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { default: axios } = require('axios');
 
-const { expect } = chai;
+const { userBalanceController } = require('./controllers/user-balance.controller');
 
-describe("Test", () => {
-  it("Basic Test", async () => {
-    expect(1).to.equal(1);
+describe('User balance controller', () => {
+  let axiosGetStub;
+
+  beforeEach(() => {
+    axiosGetStub = sinon.stub(axios, 'get');
+  });
+
+  afterEach(() => {
+    axiosGetStub.restore();
+  });
+
+  it('should send Bitstamp request with correct params', async () => {
+    const mockReq = {
+      params: {
+        userId: 'user-2',
+      },
+    };
+
+    await userBalanceController(mockReq, {}, () => {});
+
+    expect(axiosGetStub.calledOnceWith('https://www.bitstamp.net/api/v2/ticker/btcusd')).to.be.true;
+  });
+
+  it('should send error response if user ID is invalid', async () => {
+    const mockReq = {
+      params: {
+        userId: 'invalid-id',
+      },
+    };
+
+    const nextSpy = sinon.spy();
+    await userBalanceController(mockReq, {}, nextSpy);
+
+    expect("User doesn't exist in cache.").to.be.equal(nextSpy.getCall(0).args[0].message);
   });
 });
